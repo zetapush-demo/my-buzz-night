10973adbb2db5b4e1756c43c8ef1709012bca424
Pacôme Françon <pacome.francon@epitech.eu>
2018-11-12 10:38:13 +0100

    create-event component's form now work

--- src/app/create-event/create-event.component.ts
+++ src/app/create-event/create-event.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+export interface MyEvent {
+	name: string;
+	address: string;
+	date: string;
+}
+
 @Component({
 	selector: 'app-create-event',
 	templateUrl: './create-event.component.html',
@@ -18,8 +24,10 @@ export class CreateEventComponent implements OnInit {
 	) {}
 
 	format_date() {
+		if (!this.dateFormGroup.value.dateCtrl)
+			return;
 		const date_timestamp = Date.parse(this.dateFormGroup.value.dateCtrl);
-		const hour_timestamp = this.dateFormGroup.value.hourCtrl;
+		const hour_timestamp = 1000 * 60 * 60 * (this.dateFormGroup.value.hourCtrl || 0);
 		const tmp = new Date(date_timestamp + hour_timestamp);
 		const d = tmp.getDate();
 		const m = tmp.getMonth() + 1; // January is 0!
@@ -30,21 +38,24 @@ export class CreateEventComponent implements OnInit {
 	}
 
 	create_event() {
-		console.log('event: ', this.eventFormGroup.value);
-		console.log('address: ', this.addressFormGroup.value);
-		console.log('date: ', this.dateFormGroup.value);
+		const event: MyEvent = {
+			name: this.eventFormGroup.value.eventCtrl,
+			address: this.addressFormGroup.value.addressCtrl,
+			date: this.dateFormGroup.value.dateCtrl
+		}
+		console.log(event);
 	}
 
 	ngOnInit() {
 		this.eventFormGroup = this.formBuilder.group({
-			eventCtrl: ['', Validators.required]
+			eventCtrl: [null, Validators.required]
 		});
 		this.addressFormGroup = this.formBuilder.group({
-			addressCtrl: ['', Validators.required]
+			addressCtrl: [null, Validators.required]
 		});
 		this.dateFormGroup = this.formBuilder.group({
-			dateCtrl: ['', Validators.required],
-			hourCtrl: ['', Validators.required],
+			dateCtrl: [null, Validators.required],
+			hourCtrl: [null, Validators.required],
 		});
 	}
 }
