2b68b00a6dfd10aa6912330b5bfa37940dc17896
Pacôme Françon <pacome.francon@epitech.eu>
2018-11-12 12:27:17 +0100

    join-event component => one basic eventID form

--- src/app/join-event/join-event.component.ts
+++ src/app/join-event/join-event.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-join-event',
-  templateUrl: './join-event.component.html',
-  styleUrls: ['./join-event.component.scss']
+	selector: 'app-join-event',
+	templateUrl: './join-event.component.html',
+	styleUrls: ['./join-event.component.scss']
 })
-export class JoinEventComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+export class JoinEventComponent {}
