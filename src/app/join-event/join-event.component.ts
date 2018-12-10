4c15790be8f001626fe45f76682cde68ee359c8a
Pacôme Françon <pacome.francon@epitech.eu>
2018-12-10 17:01:29 +0100

    remove useless event exist check

--- src/app/join-event/join-event.component.ts
+++ src/app/join-event/join-event.component.ts
@@ -1,8 +1,5 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { MatSnackBar } from '@angular/material';
-
-import { WorkerService } from '../worker.service';
 
 @Component({
 	selector: 'app-join-event',
@@ -12,19 +9,10 @@ import { WorkerService } from '../worker.service';
 export class JoinEventComponent {
 
 	constructor(
-		private worker: WorkerService,
-		private router: Router,
-		private snackBar: MatSnackBar
+		private router: Router
 	) {}
 
 	async joinEvent(eventID: string) {
-		const exists = await this.worker.api.isEventExists(eventID);
-
-		if (exists)
-			this.router.navigate([`/event/${eventID}`]);
-		else
-			this.snackBar.open(`${eventID} event doesn't exist...`, 'Ok', {
-				duration: 5000
-			});
+		this.router.navigate([`/event/${eventID}`]);
 	}
 }
